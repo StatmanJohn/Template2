cdbbf68b28ea3c40e792095fc0d8adfab5a03a94
Statman John <tadashi.koga@gmail.com>
2017-03-29 15:37:16 +0900

    Correct project name

--- analysis/data/analysis_data/read_csv.r
+++ analysis/data/analysis_data/read_csv.r
@@ -1,8 +1,8 @@
 
 library(readr)
-demog <- read_csv("~/Project/Template1/analysis/data/raw/demog.csv", 
+demog <- read_csv("~/Project/Template2/analysis/data/raw/demog.csv", 
 col_types = cols(GENDER = col_factor(levels = c("M","F")), 
                  GROUP = col_factor(levels = c("A","B", "C", "D"))))
 
-covariance <- read_csv("~/Project/Template1/analysis/data/raw/covariance.csv", 
+covariance <- read_csv("~/Project/Template2/analysis/data/raw/covariance.csv", 
                        col_types = cols(GROUP = col_factor(levels = c("A","B", "C", "D"))))
